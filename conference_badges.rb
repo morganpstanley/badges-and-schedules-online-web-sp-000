2daa7bb1db7e6b639e80d0f18f170800105c50de
Morgan Stanley <morganpstanley@gmail.com>
2019-07-03 20:19:12 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+require 'pry'
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name)
+  name1 = name.map { |person| "Hello, my name is #{person}." }
+  return name1
+end
+
+def assign_rooms(name)
+  room = 0
+  name1 = name.map do |name|
+    room += 1
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+  return name1
+end
+
+def printer(name)
+  resulta = batch_badge_creator(name)
+  resultb = assign_rooms(name)
+  i = 0
+  while i < resulta.length
+    puts resulta[i]
+    puts resultb[i]
+    i += 1
+  end
+end
